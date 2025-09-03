655bba8c385657b97289aec84cbdf678236966f9
mahoneyjustinnj <mahoneyjustin@hotmail.com>
2025-09-03 18:33:53 +0000

    Add .gitignore and GitHub connection script

---
+++ connect_to_github_and_push_pull.R
@@ -0,0 +1,44 @@
+setwd("/cloud/project/cleaning_script/read_SQL/clean_contracts_efficient_250820/cleaned_contracts")
+# this system command is same as renning from the command line
+system("git remote -v")
+# result:
+# origin	https://github.com/mahoneyjustinnj/cleaned_contracts (fetch)
+# origin	https://github.com/mahoneyjustinnj/cleaned_contracts (push)
+#means
+# origin is the name of the remote (default name for the main GitHub link).
+# the URL is the location of your GitHub repo.
+# (fetch) means it pulls updates from GitHub.
+# (push) means it sends your changes to GitHub.
+
+#check that the github repo is the 'master' (or main) branch
+system("git branch")
+# result:
+#   * master
+
+# This will fetch and merge any new commits from your GitHub repo (cleaned_contracts) into your Posit Cloud workspace.
+system("git pull origin master")
+#result was to pll in the following 3 files (below)
+# Fast-forward
+# database.sqlite3           | Bin 0 -> 114688 bytes
+# first_read_in_data250821.R | 104 +++++++++++++++
+#   grc_service_contracts.csv  | 310 +++++++++++++++++++++++++++++++++++++++++++++
+#   3 files changed, 414 insertions(+)
+# create mode 100644 database.sqlite3
+# create mode 100755 first_read_in_data250821.R
+# create mode 100755 grc_service_contracts.csv
+
+# the following gives the log of ehat just happened
+system("git log --oneline")
+
+#this gives the status of the pushes and pulls
+system("git status")
+#result:
+# On branch master
+# Your branch is up to date with 'origin/master'.
+# Untracked files:
+#   (use "git add <file>..." to include in what will be committed)
+# .gitignore
+# connect_to_github_and_push_pull.R
+# nothing added to commit but untracked files present (use "git add" to track)
+
+system("git add .gitignore connect_to_github_and_push_pull.R")
